de8c015b002bd17544a8264d506953fc122f585d
dshuais <1137896420@qq.com>
2024-03-22 11:22:49 +0800

    ✨ feat: 新增测试用例17

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+// https://github.com/dshuais/danmaku-vue/issues/17
+import Danmaku from 'danmaku-vue'
+import { ref } from 'vue'
+
+type Dm = {
+  id: number
+  text: string
+}
+
+const dms = ref<Dm[]>([
+  { id: 1, text: 'test1' },
+  { id: 2, text: 'test2' },
+  { id: 3, text: 'test3' },
+  { id: 4, text: 'test4' },
+  { id: 5, text: 'test5' },
+])
+
+function handleChange(index: number) {
+  dms.value[index].text += '+'
+}
+
+</script>
+
+<template>
+  <Danmaku class="dm-container" :danmus="dms" useSlot loop>.
+    <template #dm="{ danmu, index }">
+      <div>{{ danmu.text }}</div>
+      <button @click="handleChange(index)">add</button>
+    </template>
+  </Danmaku>
+
+</template>
+
+<style scoped>
+.dm-container {
+  width: 100%;
+  height: 500px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: dushuai
+ * @Date: 2024-03-22 11:09:16
+ * @LastEditors: dushuai
+ * @LastEditTime: 2024-03-22 11:17:46
+ * @description: 心平气和
+ */
+// import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
